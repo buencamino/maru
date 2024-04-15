db321acf899fbc60e4dd8aee3e90c129422bd87a
michael.francis.lau <michael.francis.lau@accenture.com>
2024-04-15 10:25:56 +0800

    first commit

---
+++ src/main/java/com/maru/maru/MaruApplication.java
@@ -0,0 +1,14 @@
+package com.maru.maru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaruApplication {
+//test
+	public static void main(String[] args) {
+		System.out.println("test");
+		SpringApplication.run(MaruApplication.class, args);
+	}
+
+}
